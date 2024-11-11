c4161cbbd8f2cbe3f94d764848dc4d128bce12c8
shivam kumar singh <shivamrathore513@gmail.com>
2024-11-11 14:11:00 +0530

    chor : remove router from authroutes , because we can't use more than one router in one application

--- src/client/components/AuthRoutes.js
+++ src/client/components/AuthRoutes.js
@@ -1,22 +1,14 @@
-// src/client/components/AuthRoutes.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Auth/Login';
 import Signup from './Auth/Signup';
 
 const AuthRoutes = ({ basePath = '/auth' }) => {
     return (
-        <Router>
-            <Routes>
-                {/* Route for Login page */}
-                <Route path={`${basePath}/login`} element={<Login />} />
-
-                {/* Route for Signup page */}
-                <Route path={`${basePath}/signup`} element={<Signup />} />
-
-                {/* Optionally, you can add other auth-related routes here */}
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path={`${basePath}/login`} element={<Login />} />
+            <Route path={`${basePath}/signup`} element={<Signup />} />
+        </Routes>
     );
 };
 
